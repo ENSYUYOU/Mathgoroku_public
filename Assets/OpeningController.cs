1b9d4d87b085d001ca31164e29bc8951f0a161e2
CohnosK <matsuri.passion1015@gmail.com>
2023-03-19 19:01:33 +0900

    オープニング実装途中その2

--- Assets/OpeningController.cs
+++ Assets/OpeningController.cs
@@ -7,12 +7,36 @@ using UnityEngine.SceneManagement;
 
 public class OpeningController : MonoBehaviour
 {
-    public GameObject Opening;
     public GameObject OpeningLogo;
+    public GameObject OpeningBack;
+    public GameObject GamePreStartButton;
+    public Button GamePreStart;
+    public GameObject GameStartButton;
+    public Button GameStart;
+    
+    public AudioSource audioSource;//オーディオソースは透明なゲームオブジェクトについてる。
+    public AudioClip StartBGM;//BGM用のpublic変数
 
-    void Start()
-    {
-        
+    void Start(){
+        Invoke("StartButton", 2f);
+        audioSource.clip = StartBGM;
+        audioSource.Play();
+    }
+
+    public void StartButton(){
+        GamePreStartButton.SetActive(true);
+    }
+
+    public AudioClip selectSound;//ボタン選択時の音
+    public void StartSwitch1(){
+        audioSource.PlayOneShot(selectSound);
+        GamePreStartButton.SetActive(false);
+        GameStartButton.SetActive(true);
+        Invoke("StartSwitch2",0.25f);
+    }
+
+    public void StartSwitch2(){
+
     }
 
     /*void Update()
