c85a6a7cf50e3e6a2edbd461f4b02bc119b5053b
16-bit <mail173451@icloud.com>
2023-03-21 23:52:53 +0900

    エンディング雰囲気完成

--- Assets/EndingController.cs
+++ Assets/EndingController.cs
@@ -8,9 +8,41 @@ public class EndingController : MonoBehaviour
     public GameObject player2;
     public GameObject player3;
     public GameObject player4;
+
+    public GameObject Panel1st;
+    public GameObject Panel2nd;
+    public GameObject Panel3rd;
+    public GameObject Panel4th;
+    public GameObject Result;
     // Start is called before the first frame update
-    void Start()
+    public AudioSource SoundEffect;//オーディオソースは透明なゲームオブジェクトについてる。
+    public AudioClip Don;
+    public AudioClip Hue;
+    public AudioClip Kansei;
+
+    public Animator orange;
+    public Animator red;
+    public Animator blue;
+    public Animator yellow;
+    IEnumerator Start()
     {
+        SoundEffect.PlayOneShot(Hue);
+        yield return new WaitForSeconds(1f);
+        Panel4th.SetActive(true);
+        SoundEffect.PlayOneShot(Don);
+        yield return new WaitForSeconds(1f);
+        Panel3rd.SetActive(true);
+        SoundEffect.PlayOneShot(Don);
+        yield return new WaitForSeconds(1f);
+        Panel2nd.SetActive(true);
+        SoundEffect.PlayOneShot(Don);
+        yield return new WaitForSeconds(1f);
+        Panel1st.SetActive(true);
+        SoundEffect.PlayOneShot(Don);
+        yield return new WaitForSeconds(1f);
+        Result.SetActive(false);
+        SoundEffect.PlayOneShot(Kansei);
+        orange.SetBool("cheer", true);
         
     }
 
